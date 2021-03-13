aacf1e4658c69c40a1e95991117b37a1c80c4f7c
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-13 13:21:32 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string entrada; 
+           double milhas, quilometros; 
+
+           Console.Write("Entre com a medida (em milhas):");
+           entrada = Console.ReadLine(); 
+
+           milhas = Convert.ToDouble(entrada);
+           quilometros = milhas * 1609;
+
+           Console.WriteLine("\nEquivalente em quilômetros: ");
+           Console.WriteLine($"{quilometros:N2} km");
+
+
+
+        }
+    }
+}
